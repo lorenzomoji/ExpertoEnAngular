7feee86317fba012d0d0984dd6b944f0eade22b8
Lorenzo Moreno Ossowsky <lmoreno@corp.optaresolutions.com>
2020-10-05 14:52:59 +0200

    Subiendo single page

---
+++ singlePageApplication/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+const APP_ROUTES: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'heroes', component: HeroesComponent},
+    {path: '**', pathMatch: 'full',  redirectTo: ''}
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES );
